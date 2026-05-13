9682d3d28d9d364e77b77c600ba6e73e75483dc8
CreatorWA <rohoyoja@gmail.comgit push origin main>
2026-05-14 04:25:23 +0900

    기업형 웹 프로젝트

--- 1.pc/sub1/js/map_one.js
+++ 1.pc/sub1/js/map_one.js
@@ -1,27 +1,16 @@
-window.onload = function () {
+// 
+kakao.maps.load(function() {
+    // 지도를 생성하는 코드를 이 안에 넣으면 카카오가 준비된 후 실행돼!
     var container = document.getElementById('map');
     var options = {
-        center: new kakao.maps.LatLng(37.43800919778249, 127.00579670885342), /* 위도 경도 */
-        level: 2  /* 지도확대 */
+        center: new kakao.maps.LatLng(37.43800919778249, 127.00579670885342),
+        level: 2
     };
-
     var map = new kakao.maps.Map(container, options);
-
-    var mapTypeControl = new kakao.maps.MapTypeControl();
-    map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
-
-    var zoomControl = new kakao.maps.ZoomControl();
-    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
     var markerPosition = new kakao.maps.LatLng(37.43800919778249, 127.00579670885342);
     var marker = new kakao.maps.Marker({
         position: markerPosition
     });
-
     marker.setMap(map);
-
-    var overlay = new kakao.maps.CustomOverlay({
-        map: map,
-        position: marker.getPosition()
-    });
-}
+});
