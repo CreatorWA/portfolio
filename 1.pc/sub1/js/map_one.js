b34e61c45ae5b1af0f00549504f8b7faeb527061
CreatorWA <rohoyoja@gmail.comgit push origin main>
2026-05-14 04:38:12 +0900

    기업형 웹 프로젝트

--- 1.pc/sub1/js/map_one.js
+++ 1.pc/sub1/js/map_one.js
@@ -1,27 +1,58 @@
+// window.onload = function () {
+//     var container = document.getElementById('map');
+//     var options = {
+//         center: new kakao.maps.LatLng(37.43800919778249, 127.00579670885342), /* 위도 경도 */
+//         level: 2  /* 지도확대 */
+//     };
+
+//     var map = new kakao.maps.Map(container, options);
+
+//     var mapTypeControl = new kakao.maps.MapTypeControl();
+//     map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+//     var zoomControl = new kakao.maps.ZoomControl();
+//     map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+//     var markerPosition = new kakao.maps.LatLng(37.43800919778249, 127.00579670885342);
+//     var marker = new kakao.maps.Marker({
+//         position: markerPosition
+//     });
+
+//     marker.setMap(map);
+
+//     var overlay = new kakao.maps.CustomOverlay({
+//         map: map,
+//         position: marker.getPosition()
+//     });
+
+
+
+
 window.onload = function () {
-    var container = document.getElementById('map');
-    var options = {
-        center: new kakao.maps.LatLng(37.43800919778249, 127.00579670885342), /* 위도 경도 */
-        level: 2  /* 지도확대 */
-    };
+    kakao.maps.load(function() {  // 로딩 완료 후 실행
+        var container = document.getElementById('map');
+        var options = {
+            center: new kakao.maps.LatLng(37.43800919778249, 127.00579670885342),
+            level: 2
+        };
+        var map = new kakao.maps.Map(container, options);
 
-    var map = new kakao.maps.Map(container, options);
+        var mapTypeControl = new kakao.maps.MapTypeControl();
+        map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
 
-    var mapTypeControl = new kakao.maps.MapTypeControl();
-    map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+        var zoomControl = new kakao.maps.ZoomControl();
+        map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
-    var zoomControl = new kakao.maps.ZoomControl();
-    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+        var markerPosition = new kakao.maps.LatLng(37.43800919778249, 127.00579670885342);
+        var marker = new kakao.maps.Marker({ position: markerPosition });
+        marker.setMap(map);
 
-    var markerPosition = new kakao.maps.LatLng(37.43800919778249, 127.00579670885342);
-    var marker = new kakao.maps.Marker({
-        position: markerPosition
+        // CustomOverlay 완성 예시 (content 추가)
+        var overlay = new kakao.maps.CustomOverlay({
+            map: map,
+            position: marker.getPosition(),
+            content: '<div style="padding:5px; background:white; border:1px solid black;">오버레이 내용</div>',
+            yAnchor: 1
+        });
     });
-
-    marker.setMap(map);
-
-    var overlay = new kakao.maps.CustomOverlay({
-        map: map,
-        position: marker.getPosition()
-    });
-}
+};
