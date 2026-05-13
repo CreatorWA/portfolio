7736a59ced58e5c1528af18c5372b197dea5e2e6
CreatorWA <rohoyoja@gmail.comgit push origin main>
2026-05-14 03:41:19 +0900

    기업형 웹 프로젝트

--- 1.pc/sub1/js/map_one.js
+++ 1.pc/sub1/js/map_one.js
@@ -1,26 +1,26 @@
 window.onload = function () {
     var container = document.getElementById('map');
     var options = {
-        center: new daum.maps.LatLng(37.43800919778249, 127.00579670885342), /* 위도 경도 */
+        center: new kakao.maps.LatLng(37.43800919778249, 127.00579670885342), /* 위도 경도 */
         level: 2  /* 지도확대 */
     };
 
-    var map = new daum.maps.Map(container, options);
+    var map = new kakao.maps.Map(container, options);
 
-    var mapTypeControl = new daum.maps.MapTypeControl();
-    map.addControl(mapTypeControl, daum.maps.ControlPosition.TOPRIGHT);
+    var mapTypeControl = new kakao.maps.MapTypeControl();
+    map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
 
-    var zoomControl = new daum.maps.ZoomControl();
-    map.addControl(zoomControl, daum.maps.ControlPosition.RIGHT);
+    var zoomControl = new kakao.maps.ZoomControl();
+    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
-    var markerPosition = new daum.maps.LatLng(37.43800919778249, 127.00579670885342);
-    var marker = new daum.maps.Marker({
+    var markerPosition = new kakao.maps.LatLng(37.43800919778249, 127.00579670885342);
+    var marker = new kakao.maps.Marker({
         position: markerPosition
     });
 
     marker.setMap(map);
 
-    var overlay = new daum.maps.CustomOverlay({
+    var overlay = new kakao.maps.CustomOverlay({
         map: map,
         position: marker.getPosition()
     });
